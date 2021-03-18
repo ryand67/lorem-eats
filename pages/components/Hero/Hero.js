26f955df4a0f1ae70412cdb699700d90f4889e44
ryand67 <rwdevenney@gmail.com>
2021-03-18 18:12:45 -0400

    nav and hero blurb

--- pages/components/Hero/Hero.js
+++ pages/components/Hero/Hero.js
@@ -6,8 +6,9 @@ export default function Hero() {
         <div className={styles.hero}>
             <div className={styles.heroShader}>
                 <h1 className={styles.header}>Lorem Eats</h1>
-                <hr className={styles.hr}/>
-                <button className={styles.aboutBtn}>About</button>
+                {/* <hr className={styles.hr}/>
+                <button className={styles.aboutBtn}>About</button> */}
+                <p className={styles.blurb}>Classic and authentic steaks</p>
             </div>
         </div>
     )
